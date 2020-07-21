4bab4b89d6c4db039465ba1a032d135102d1eb6b
julianramirezch <julianramirezch1@gmail.com>
2020-07-21 15:49:47 -0500

    [fix]: order

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,7 +12,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (!tree->right && !tree->left)
+	if (!tree->left && !tree->right)
 		return (1);
 
 	if (binary_tree_balance(tree) == 0)
