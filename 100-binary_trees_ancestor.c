d5d22d908fd13b4d92266dcf9e8a2812fd36f8b6
julianramirezch <julianramirezch1@gmail.com>
2020-07-22 18:42:46 -0500

    [refactor]

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -16,54 +16,15 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	if (!first || !second)
 		return (NULL);
 
-	depth_a = binary_tree_depth((binary_tree_t *)first);
-	depth_b = binary_tree_depth((binary_tree_t *)second);
-
-	if (depth_a == depth_b)
-		return ((binary_tree_t *)first->parent);
-
 	first_copy = (binary_tree_t *)first;
 	second_copy = (binary_tree_t *)second;
 
-	if (depth_b == depth_a + 1)
+	while (first_copy && second_copy)
 	{
-		if (second_copy->parent == first_copy)
+		if (first_copy == second_copy)
 			return (first_copy);
-		if (second_copy->parent->parent == first_copy->parent)
-			return (first_copy->parent);
+		first_copy = first_copy->parent;
+		second_copy = second_copy->parent;
 	}
-
-	if (depth_a == depth_b + 1)
-	{
-		if (first_copy->parent == second_copy)
-			return (second_copy);
-		if (first_copy->parent->parent == second_copy->parent)
-			return (second_copy->parent);
-	}
-
 	return (NULL);
-}
-
-/**
- * binary_tree_depth - measures the depth of a node in a binary tree
- * @tree: pointer to the node to measure the depth
- * Return: depth of the binary tree
- */
-
-size_t binary_tree_depth(const binary_tree_t *tree)
-{
-	const binary_tree_t *aux;
-	size_t depth = 0;
-
-	if (!tree)
-		return (0);
-
-	aux = tree;
-	while (aux->parent)
-	{
-		aux = aux->parent;
-		depth += 1;
-	}
-
-	return (depth);
 }
