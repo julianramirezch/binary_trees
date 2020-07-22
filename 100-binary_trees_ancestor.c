32b9309d9d5fd0da172348cefd5660680537e18e
julianramirezch <julianramirezch1@gmail.com>
2020-07-22 18:48:56 -0500

    [add]: New condition second_copy

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -24,6 +24,8 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 			return (first_copy);
 		if (first_copy == second_copy->parent)
 			return (first_copy);
+		if (second_copy == first_copy->parent)
+			return (second_copy);
 		if (first_copy->parent == second_copy->parent->parent)
 			return (first_copy->parent);
 		first_copy = first_copy->parent;
